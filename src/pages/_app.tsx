b2ad7c4757ff3bbbc3bba6551133571e8ff0edbf
syed zunair gillani <zunairgillani54@gmail.com>
2023-11-20 18:44:37 +0500

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import Footer from '@/components/footer'
 import Header from '@/components/header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
   const {asPath} = useRouter()
   return <>
+    <Head>
+      <title>Packhoop</title>
+    </Head>
     {!asPath.includes('studio') && <Header />}
     <Component {...pageProps} />
     {!asPath.includes('studio') && <Footer />}
