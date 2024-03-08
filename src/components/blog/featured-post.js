0983cddc7abb0b8173d7db8be33b882fcd914435
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-03-08 16:03:38 +0500

    Blogs

--- src/components/blog/featured-post.js
+++ src/components/blog/featured-post.js
@@ -5,59 +5,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Featured_Posts = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }
-        ]
-    };
-    const slider = React.useRef(null);
+
     return (
-        <div className='relative'>
-            <Slider ref={slider} {...settings} className='h-full'>
-                {
-                    [0, 1, 2, 3, 4].map((item, idx) => (
-                        <div key={idx} className='md:pr-10'>
-                            <Image src="/images/make-product.png" alt='make-product' width={793} height={472} className='w-full' />
-                            <div className='mt-3'>
-                                <Link href="#" className='md:text-2xl text-sm font-medium text-title_Clr text-center block w-fit mx-auto'>
-                                    Lorem ipsum dolor sit amet
-                                </Link>
-                                <p className='md:text-base text-xs font-medium text-txt_Clr text-center'>
-                                    2 comments . DEC 19, 2024
-                                </p>
-                            </div>
-                        </div>
-                    ))
-                }
-            </Slider>
-            <button className="absolute md:hidden block top-1/2 left-0 border rounded-full p-1 transform -translate-y-1/2 text-base bg-primary text-white " onClick={() => slider?.current?.slickPrev()}><IoIosArrowBack /></button>
-            <button className="absolute md:hidden block top-1/2 right-0 border rounded-full p-1 transform -translate-y-1/2 text-base bg-primary text-white " onClick={() => slider?.current?.slickNext()}><IoIosArrowForward /></button>
+        <div className='shadow-[0px_8px_5px_0px_rgb(23,43,77,8%)] hover:shadow-[0px_3px_2px_0px_rgb(23,43,77,8%)] rounded-lg'>
+            <div>
+                <Image src="/images/make-product.png" alt='make-product' width={793} height={472} className='w-full h-full object-cover rounded-t-lg' />
+            </div>
+            <div className='py-4'>
+                <Link href="#" className='md:text-2xl text-base font-medium text-title_Clr text-center block w-fit mx-auto'>
+                    Lorem ipsum dolor sit amet
+                </Link>
+                <p className='md:text-base text-sm font-medium text-txt_Clr text-center'>
+                    2 comments . DEC 19, 2024
+                </p>
+            </div>
         </div>
     )
 }
--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -11,30 +11,29 @@ export default function Blogs() {
             <section className='md:py-16 py-8'>
                 <div className='container mx-auto px-4 flex md:flex-row flex-col gap-7 md:divide-x divide-txt_Clr/50'>
                     <div className='md:w-2/3 w-full'>
-                        {/* <h2 className='md:text-4xl text-xl font-semibold text-title_Clr mb-5'>
-                            Our Blog
-                        </h2> */}
-                        <div>
-                            <Featured_Posts />
-                        </div>
-                    </div>
-                    <div className='md:w-1/3 w-full md:pl-10 md:block block'>
-                        {/* <p className='text-end'>
-                            <Link href="#" className='md:text-base text-xs font-medium text-primary underline hover:no-underline'>
-                                View all
-                            </Link>
-                        </p>
-                        <h2 className='md:text-3xl text-xl font-semibold text-title_Clr mb-5'>
-                            Latest posts
-                        </h2> */}
-                        <div className='grid gap-5'>
+                        <h2 className='md:text-4xl text-xl font-semibold text-title_Clr mb-5'>
+                            All Posts
+                        </h2>
+                        <div className='grid md:grid-cols-2 grid-cols-1 gap-7'>
                             {[0, 1, 2, 3].map(({ item, idx }: any) => {
                                 return (
-                                    <div key={idx} className='flex md:flex-row flex-col gap-2'>
+                                    <Featured_Posts key={idx} />
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <div className='md:w-1/3 w-full md:pl-7 md:block block'>
+                        <h2 className='md:text-3xl text-xl font-semibold text-title_Clr mb-5'>
+                            Latest posts
+                        </h2>
+                        <div className='grid gap-7'>
+                            {[0, 1, 2, 3].map(({ item, idx }: any) => {
+                                return (
+                                    <div key={idx} className='flex md:flex-row flex-row gap-2 shadow-[0px_8px_5px_0px_rgb(23,43,77,8%)] hover:shadow-[0px_3px_2px_0px_rgb(23,43,77,8%)] rounded'>
                                         <div>
-                                            <Image src="/images/make-product.png" alt='make-product' width={144} height={89} className='h-full w-full object-cover' />
+                                            <Image src="/images/make-product.png" alt='make-product' width={144} height={89} className='h-full w-full object-cover rounded-l' />
                                         </div>
-                                        <div className=''>
+                                        <div className='py-4'>
                                             <Link href="#" className='md:text-xl text-base font-medium text-title_Clr block w-fit'>
                                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                                             </Link>
