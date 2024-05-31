5f79674fa06f583bbc6786dee7eeb48cf22fbe3d
syed-zunair-gillani <zunairgillani54@gmail.com>
2024-05-31 20:07:45 +0500

    update

--- src/components/home/designBox.tsx
+++ src/components/home/designBox.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 function DesignBox({ title, img, slug }: any) {
     return (
-        <Link href={`/${slug?.current}`}>
+        <Link href={`/${slug?.current}`} className='border pb-3 group'>
             <div className=''>
-                <Image src={img} alt='pack/1' width={306} height={340} className='mx-auto cursor-pointer mb-4' />
+                <Image src={img} alt='pack/1' width={306} height={340} className='mx-auto cursor-pointer mb-4 group-hover:scale-105 transition-all duration-150 ease-linear' />
             </div>
             <div className='block mx-auto md:text-lg text-base font-semibold text-title_Clr text-center'>
                 {title}
